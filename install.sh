e0bc2579a83911208d6216d7dbdcfb495a9f0d71
darmadevZone <darmadevdone@gmail.com>
2024-03-30 17:41:43 +0900

    修正

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ fi
 if ! command -v fish >/dev/null 2>&1; then
     echo "Installing fish..."
     brew install fish
-    sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
+    sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
     chsh -s /opt/homebrew/bin/fish
     echo "Add brew binaries in fish path run the command..."
     fish_add_path /opt/homebrew/bin
