518eb6f9d633c24d2ec99a428d6339627829fb9d
darmadevZone <darmadevdone@gmail.com>
2024-03-30 17:25:32 +0900

    Rosetta2,homebrew,fish,mise,weztermのインストール

---
+++ README.md
@@ -0,0 +1,10 @@
+# dotfilesを作成してみる
+
+- CheckOS
+
+## インストールするもの
+- Rosetta2
+- Homebrew
+    - fish(shell)
+    - wezterm(terminal)
+    - mise(runtime manager)
---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -eu
+
+
+# Check OS
+if [ "$(uname)" != "Darwin" ] ; then
+	echo "Not macOS!"
+	exit 1
+fi
+
+
+# Install Rosetta 2 for Apple Silicon
+if [ "$(uname -m)" = "arm64" ] ; then
+  /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+fi
+
+# Install Homebrew
+if ! command -v brew >/dev/null 2>&1; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "Install brew successfully!"
+  brew install cask
+  echo "Install brew cask successfully!"
+fi
+
+## Install fish
+if ! command -v fish >/dev/null 2>&1; then
+    echo "Installing fish..."
+    brew install fish
+    sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
+    chsh -s /opt/homebrew/bin/fish
+    echo "Add brew binaries in fish path run the command..."
+    fish_add_path /opt/homebrew/bin
+fi
+
+## Install mise
+if ! command -v mise >/dev/null 2>&1; then
+    echo "Installing mise..."
+    brew install mise
+fi
+
+## Install wezterm
+if ! command -v wezterm >/dev/null 2>&1; then
+    echo "Installing wezterm..."
+    brew install --cask wezterm
+fi
+
+printf '%s\n' $fish_user_paths
